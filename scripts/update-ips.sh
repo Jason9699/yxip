3128dd5fdca36afa29ce019e00394d858d9da2b3
Jason9699 <x15041669699@gmail.com>
2025-06-06 21:34:19 +0800

    Delete scripts directory

--- scripts/update-ips.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "更新 Cloudflare IP 列表..."
-wget -q -O ip-lists/cloudflare-ips.txt https://www.cloudflare.com/ips-v4
-echo "保留现有非 Cloudflare IP 列表..."
-touch ip-lists/non-cloudflare-ips.txt
